bc343f60a9b956b61672c19989a26910f8fce82a
kinggggg <zeek_beijing@126.com>
2022-07-19 15:22:02 +0800

    world count程序

---
+++ src/main/java/com/zeek/flink/learnflink/LearnFlinkApplication.java
@@ -0,0 +1,13 @@
+package com.zeek.flink.learnflink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnFlinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeek/flink/learnflink/LearnFlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeek.flink.learnflink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnFlinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
